f20c73e392656c65dbf918a32e7407a772403ad9
crusom <olaf.grzybowski.1@gmail.com>
2023-03-19 12:04:41 +0100

    added days argument

--- scripts/renew_cert.sh
+++ scripts/renew_cert.sh
@@ -2,7 +2,7 @@
 
 #usage: ./cert_renew.sh <key file> <cert file>
 
-if [ "$#" -lt 2 ]
+if [ "$#" -lt 3 ]
 then
     echo "$0 <key file> <cert file> <number of days>"
     exit 1
@@ -11,11 +11,12 @@ fi
 # First arg is pointed to current PEM file
 key_file="$1"
 cert_file="$2"
+number_of_days=$3
 
 # Create temporary files
 tmp_csr=$(mktemp /tmp/csr.XXXXXXXXX)
 
 openssl x509 -x509toreq -in $cert_file -signkey $key_file -out $tmp_csr
-openssl x509 -req -days 213700 -in $tmp_csr -signkey $key_file -out cert_new.pem
+openssl x509 -req -days $number_of_days -in $tmp_csr -signkey $key_file -out cert_new.pem
 echo "new cert is in cert_new.pem"
 rm $tmp_csr 
